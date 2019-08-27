e97a76dfa38ee7921809645a7c71c97c465f6bee
Jeff Hagelberg <jnhagelberg@us.ibm.com>
2019-08-27 10:37:28 -0400

    egeria#863 - Do not commit message offsets in kafka consumer until the message has been completely processed by all consumers.
    Signed-off-by: Jeff Hagelberg <jnhagelberg@us.ibm.com>

--- open-metadata-implementation/adapters/open-connectors/event-bus-connectors/open-metadata-topic-connectors/kafka-open-metadata-topic-connector/src/main/java/org/odpi/openmetadata/adapters/eventbus/topic/kafka/KafkaOpenMetadataEventConsumerProperty.java
+++ open-metadata-implementation/adapters/open-connectors/event-bus-connectors/open-metadata-topic-connectors/kafka-open-metadata-topic-connector/src/main/java/org/odpi/openmetadata/adapters/eventbus/topic/kafka/KafkaOpenMetadataEventConsumerProperty.java
@@ -36,7 +36,23 @@ public enum KafkaOpenMetadataEventConsumerProperty
 	/*
 	 * The amount of time to sleep after we encounter a Kafka error, in seconds
 	 */
-	RECOVERY_SLEEP_TIME("recovery_sleep_time_sec", "10");
+	RECOVERY_SLEEP_TIME("recovery_sleep_time_sec", "10"),
+	
+    /**
+     * This property adds an optional upper bound for the consumer event processing
+     * before an event is treated as being fully processed and committed to kafka.  If
+     * the value is negative, there is no timeout, and offsets will not be committed
+     * into kafka the message has been completely processed.
+     */
+    CONSUMER_EVENT_PROCESSING_TIMEOUT_MINS("consumer_message_processing_timout_mins", "30"),
+    
+    /**
+     * Controls the inverval between checks to see if a message has
+     * been processed and its offset committed to kafka.  This is only
+     * used if auto commit is disabled in kafka. 
+     * 
+     */
+    COMMIT_CHECK_INTERVAL_MS("commit_check_interval_ms", "5000");
 
 	private String propertyName;
 	private String defaultValue;
